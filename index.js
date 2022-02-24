2e297c796dfb494d2169452740a511330757760d
Yash Dharmal <yashdharmal8353@gmail.com>
2022-02-24 21:43:01 +0530

    changed ""

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ else if (randomNumber1 < randomNumber2) {
     document.querySelector("h1").innerHTML="🚩Player 2 Wins!"
 }
 else {
-    document.querySelector("h1").innerHTML="Draw"
+    document.querySelector("h1").innerHTML='Draw'
 }
